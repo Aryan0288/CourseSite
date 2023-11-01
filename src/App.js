979118dea772f7617ba103c1488e72a79af9f9d5
Aryan <aryansharma31658@gmail.com>
2023-11-01 08:59:31 +0530

    this is my first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type='text' className='text-[0.5rem]'/>
+    </div>
+  );
+}
+
+export default App;
