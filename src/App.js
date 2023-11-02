0be29ef51788d6da44a454c3e05766ffeea501a6
Aryan <aryansharma31658@gmail.com>
2023-11-02 15:37:53 +0530

    api fetch properly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Navbar from './Components/Navbar';
+import Filter from './Components/Filter';
+import Cards from './Components/Cards';
+import { apiUrl,filterData } from './data';
+import Spinner from './Components/Spinner';
+import {toast} from "react-toastify";
 
-function App() {
+
+
+export default function App() {
+  const [course,setCourses]=useState('');
+  const [load,setLoad]=useState(false);
+  const [category,setCategory]=useState(filterData[0].title);
+  // async function fetchData(){
+  //   setLoad(true);
+  //   try{
+  //     let response=await fetch(apiUrl);
+  //     let output=await response.json();
+
+  //     // output
+  //     setCourses(output.data);
+  //     console.log(course);
+  //   }
+  //   catch(error){
+  //     toast.error("There Some Issue");
+  //   }
+  //   setLoad(false);
+  // }
+
+  useEffect(()=>{
+    setLoad(true);
+    const fetchData=async()=>{
+      try{
+        const res = await fetch(apiUrl);
+        const output = await res.json();
+
+        setCourses(output.data);
+      }
+      catch{
+        toast.error("something went wrong");
+      }
+      setLoad(false);
+    }
+    fetchData();
+  },[])
+
   return (
-    <div className="App">
-      <input type='text' className='text-[0.5rem]'/>
+    <div className='min-h-screen flex flex-col'>
+      <div>
+        <Navbar/>
+      </div>
+      <div>
+        <Filter filterData={filterData} category={category} setCategory={setCategory}/>
+      </div>
+      <div>
+        {
+          load ? <Spinner/>:<Cards course={course} category={category}/>
+        }
+        {/* <Cards/> */}
+      </div>
     </div>
   );
 }
-
-export default App;
